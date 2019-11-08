917abea3b5ee5147f939b6ce82f8f7be5f70b1a7
jeffnguyener <jeffnguyen.83@gmail.com>
2019-11-08 16:17:56 -0600

    call and render api first name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { createResource } from "./PersonAPi";
+import { Person } from "./Person";
+
+const resource = createResource();
 
 function App() {
-  return <div className="App">yo</div>
+  return (
+    <div className="App">
+      <Suspense fallback={<h1>loading person...</h1>}>
+        <Person resource={resource} />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
